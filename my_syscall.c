e7620bf169a7d37d9eecd441d3aaa4c43c4413e4
Christopher David Monken <cmonken@asu.edu>
2016-02-13 19:11:38 -0700

    initial custom syscall

--- my_syscall.c
+++ my_syscall.c
@@ -1,65 +1,58 @@
-/* 
- *  Christopher David Monken
- *  Project 1
+/*
+ *  Christopher David Monken, Adam Mastov, Fan, Martin Kuna
+ *  Project 2
  *  CSE430: Operating Systems
  */
 
-#include <linux/kernel.h>
 #include <linux/syscalls.h>
-#include <linux.sched.h>
+//#include <linux/fs.h>
+#include <linux/jiffies.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/tty.h>
+#include <linux/uaccess.h>
+#include <string.h>
 
-struct task_struct{
-
-	pid_t pid;
-
-	cputime_t utime, stime;
-
-	char comm[TASK_COMM_LEN];
-
-	struct signal_struct *signal;
-
+struct my_task{
+    int  task_pid;
+    char task_tty[64];
+    unsigned long task_time;
+    char task_comm[15];
 }
 
-struct signal_struct{
-
-	struct tty_struct *tty;
-
-}
-
-struct tty_struct{
-
-	char name[64];
-
-}
-
-asmlinkage long sys_my_syscall(int a, int b, char *c)
+asmlinkage long sys_my_syscall(void)
 {
-	char kernel_buff[1024];
-	int len = 0;
+	int task_list_len = 0;
+    int i = 0;                                                          // for traversing my_task_array
+    unsigned long jiff, time;
+    struct task_struct *p;                                              // pointer for iterating across task list
 
-	// put information in kernel_buff
+    for_each_process(p){
+        task_list_len++;                                                // get the task list length
+    }
 
+    struct my_task task_array[task_list_len];                           // array to pass data to user-space
 
-	for_each_process(my_task){
+    for_each_process(p){
 		cputime_t utime = 0;
 		cputime_t stime = 0;
 
-		thread_group_cputime_adjusted(tas, &utime, &stime);
+        task_array[i].task_pid = (int) p->pid;
 
+        if(p->signal->tty->name == NULL)                                // change NULL to '?' -> tty format in 'ps -e'
+            strcat(task_array[i].task_tty, "?");
+        else
+            strcpy(task_array[i].task_tty, p->signal->tty->name);
+
+        strcpy(task_array[i].task_comm, p->comm);
+
+		thread_group_cputime_adjusted(p, &utime, &stime);
 		jiff = utime + stime;
+        time = jiff / HZ;                                               // convert jiffies to seconds
+        task_array[i].task_time = time;
 
-	}
+        i++;
+    }
 
-	}
-
-
-
-	// put length of this information in variable len
-
-	if(len > b)
-		len = b;
-
-	int n = copy_to_user(c, kernel_buff, len);
-	return n;
+    // copy array to user space
 }
-
